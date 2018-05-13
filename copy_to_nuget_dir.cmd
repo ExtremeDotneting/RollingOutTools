0a3a6990ec11f13cfea7aad0321be272b451de35
abarmal <yuram1inbox@gmail.com>
2018-05-13 15:56:52 +0300

    Finally updated JsonLocalStorage, now it can work in 1+ parallel processes, because it will wait if file is busy (when saving value and because now it have writes counter.
    Except this, i add method ContainsKey and fix bug in Get method (note, that now it will return default values for struct if key not exists).
    
    Limitations:
    - It will work slower if you call any method after Set method. because write/read file operation now synchronized (in JsonLocalStorage). In previous version you could get value from RAM without awaiting write operation, but it not work fine if you use storage in two parallel processes. Anyway, if you call this methods not from ui thread, it is not a problem.

--- copy_to_nuget_dir.cmd
+++ copy_to_nuget_dir.cmd
@@ -1,2 +1,6 @@
 FORFILES  /M "*.nupkg" /S /C "cmd /c xcopy @file E:\cf\projects\LocalNugetDir\ /y /i /s
+echo !!!!Press enter if you want to delete .nupkg files from subfolders (recommendet) or close console.!!!!
+pause
+FORFILES  /M "*.nupkg" /S /C "cmd /c del @file
+echo Deleted.
 pause
