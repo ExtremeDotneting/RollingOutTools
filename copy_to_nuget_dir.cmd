1afb7546429166759752798dd8b405b86bdac517
admiralWoop <danchic2202@gmail.com>
2018-06-15 03:58:15 +0700

    Fixed two things

--- copy_to_nuget_dir.cmd
+++ copy_to_nuget_dir.cmd
@@ -1,5 +1,6 @@
-FORFILES  /M "*.nupkg" /S /C "cmd /c xcopy @file E:\cf\projects\LocalNugetDir\ /y /i /s
-echo !!!!Press enter if you want to delete .nupkg files from subfolders (recommendet) or close console.!!!!
+@echo off
+FORFILES  /M "*.nupkg" /S /C "cmd /c xcopy @file D:\Programing\LocalNuget /y /i /s
+echo !!!!Press enter if you want to delete .nupkg files from subfolders (recommended) or close console.!!!!
 pause
 FORFILES  /M "*.nupkg" /S /C "cmd /c del @file
 echo Deleted.
