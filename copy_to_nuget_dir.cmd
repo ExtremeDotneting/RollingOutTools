f40da0c5f2282dd016e7c7615245d1cd0671c44a
abarmal <yuram1inbox@gmail.com>
2018-06-15 22:47:45 +0300

    Removed unused assemblies.

--- copy_to_nuget_dir.cmd
+++ copy_to_nuget_dir.cmd
@@ -1,3 +1,4 @@
+@echo off
 FORFILES  /M "*.nupkg" /S /C "cmd /c xcopy @file E:\cf\projects\LocalNugetDir\ /y /i /s
 echo !!!!Press enter if you want to delete .nupkg files from subfolders (recommendet) or close console.!!!!
 pause
