42ac8c92f5091223161435f597ea9499d3faa821
abarmal <yuram1inbox@gmail.com>
2018-09-18 15:09:27 +0300

    Minor script update.

--- copy_to_nuget_dir.cmd
+++ copy_to_nuget_dir.cmd
@@ -1,7 +1,11 @@
 @echo off
-FORFILES  /M "*.nupkg" /S /C "cmd /c xcopy @file E:\cf\projects\LocalNugetDir\ /y /i /s 
-echo !!!!Press enter if you want to delete .nupkg files from subfolders (recommendet) or close console.!!!!
+echo Founded files:
+FORFILES /M "*.nupkg" /S /C "cmd /c echo @file"
 pause
-FORFILES  /M "*.nupkg" /S /C "cmd /c del @file
+FORFILES /M "*.nupkg" /S /C "cmd /c xcopy @file E:\cf\projects\LocalNugetDir\ /y /i /s "
+echo Copied!
+echo Press enter if you want to delete .nupkg files from subfolders (recommendet) or close console.
+pause
+FORFILES /M "*.nupkg" /S /C "cmd /c del @file"
 echo Deleted.
 pause
