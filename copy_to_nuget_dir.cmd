ffccc62a06cd89b19d564464bff2054ecc90a5ab
abarmal <yuram1inbox@gmail.com>
2018-09-21 19:33:32 +0300

    Added delete_nupkgs.cmd file.

--- copy_to_nuget_dir.cmd
+++ copy_to_nuget_dir.cmd
@@ -4,8 +4,5 @@ FORFILES /M "*.nupkg" /S /C "cmd /c echo @file"
 pause
 FORFILES /M "*.nupkg" /S /C "cmd /c xcopy @file E:\cf\projects\LocalNugetDir\ /y /i /s "
 echo Copied!
-echo Press enter if you want to delete .nupkg files from subfolders (recommendet) or close console.
-pause
-FORFILES /M "*.nupkg" /S /C "cmd /c del @file"
-echo Deleted.
+call delete_nupkgs.cmd
 pause
